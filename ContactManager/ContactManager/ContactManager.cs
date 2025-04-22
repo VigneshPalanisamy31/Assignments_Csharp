c73e7ac2f752a5905c3ed4a099113a4f258e92a3
VigneshPalanisamy31 <vignesh.palanisamy@solitontech.com>
2025-04-22 11:45:06 +0530

    feat: develop a  function to create new contacts

--- ContactManager/ContactManager/ContactManager.cs
+++ ContactManager/ContactManager/ContactManager.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ContactsManager;
 
 namespace ContactManager
 {
-    internal class Class1
+    internal class ContactManager
     {
+        List<Contact> Contacts = new List<Contact>();
+        Validator isValid = new Validator();
+        /// <summary>
+        /// Function to add new contacts.
+        /// </summary>
+        public void AddNewContact()
+        {
+            Console.WriteLine("Enter the Name :");
+            string Name = isValid.ValidateName(Contacts, Console.ReadLine());
+            Console.WriteLine("Enter the Phone Number :");
+            string PhoneNumber = isValid.ValidatePhoneNumber(Console.ReadLine());
+            Console.WriteLine("Enter the Mail id :");
+            string Email = isValid.ValidateEmail(Console.ReadLine());
+            Console.WriteLine("Add Some Notes :");
+            string Notes = Console.ReadLine();
+            Contacts.Add(new Contact(Name, PhoneNumber, Email, Notes));
+            Contacts = Contacts.OrderBy(c => c.Name).ToList();
+            Console.WriteLine("Contact Created Successfully.....");
+        }
+
     }
 }
