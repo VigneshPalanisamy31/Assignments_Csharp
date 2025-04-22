5d3a74942b05f5bc1b5d42bda896197068378317
VigneshPalanisamy31 <vignesh.palanisamy@solitontech.com>
2025-04-22 10:38:51 +0530

    feat: add Contact class to create a contact template

---
+++ ContactManager/ContactManager/Contact.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsManager
+{
+    internal class Contact
+    {
+        string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        string _phoneNumber;
+        public string PhoneNumber
+        {
+            get { return _phoneNumber; }
+            set { _phoneNumber = value; }
+        }
+        string _email;
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+        string _notes;
+        public string Notes
+        {
+            get { return _notes; }
+            set { _notes = value; }
+        }
+
+        public Contact(string name, string phoneNumber, string email, string notes)
+        {
+            _name = name;
+            _phoneNumber = phoneNumber;
+            _email = email;
+            _notes = notes;
+        }
+      
+    }
+}
