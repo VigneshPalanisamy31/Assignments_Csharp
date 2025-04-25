e8f8a5d4b7fe9c5363acfdb4926340f80af9ea00
VigneshPalanisamy31 <vignesh.palanisamy@solitontech.com>
2025-04-25 14:57:24 +0530

    feat: develop a product class as a template for inventory products with required fields

---
+++ Assignment-3/Inventory Manager/Inventory Manager/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_Management
+{
+    internal class Product
+    {
+        int _productID;
+        public int ProductID { get => _productID; set => _productID = value; }
+
+        string _productname;
+        public string ProductName { get => _productname; set => _productname = value; }
+
+        double _price;
+        public double Price { get => _price; set => _price = value; }
+
+        int _quantityInStock;
+        public int QuantityInStock { get => _quantityInStock; set => _quantityInStock = value; }
+
+        public Product(int productID, string productname, double price, int quantityInStock)
+        {
+            ProductID = productID;
+            ProductName = productname;
+            Price = price;
+            QuantityInStock = quantityInStock;
+        }
+    }
+}
