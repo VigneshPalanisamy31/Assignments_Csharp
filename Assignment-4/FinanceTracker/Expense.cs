19a4a9154d3cd95165d815cf8dd4cb33a5f6e461
VigneshPalanisamy31 <vignesh.palanisamy@solitontech.com>
2025-05-02 12:25:18 +0530

    feat: develop finance tracker functionalities for existing user

---
+++ Assignment-4/FinanceTracker/Expense.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceTracker
+{
+    internal class Expense
+    {
+        public static void ExpenseFunctions(FinanceTransactions financer, string name, string filepath)
+        {
+
+
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("\n1.Add Expense Transaction\n2.Edit Expense Transaction\n3.View Expense Stats\n4.Delete Expense Transaction\n5.Exit");
+                int _choice = Validation.GetValidInteger("your choice");
+
+                switch (_choice)
+                {
+                    case 1:
+                        financer.AddTransaction(UserInteract.GetUserInput(name, "expense category"), filepath, "Expense");
+                        Console.WriteLine("Expense Tracked Successfully....");
+                        break;
+
+                    case 2:
+                        financer.EditTransaction(name, filepath, "Expense");
+                        break;
+
+                    case 3:
+                        financer.ViewTransaction(name, filepath, "Expense");
+                        break;
+
+                    case 4:
+                        financer.DeleteTransaction(name, filepath, "Expense");
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Exiting....");
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Please choose from given options");
+                        break;
+
+
+                }
+                Console.WriteLine("Press any key to continue.....");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
