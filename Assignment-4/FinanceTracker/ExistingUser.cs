73f97d33ea2ce73edd29ff43432a2949ae5d29b8
VigneshPalanisamy31 <vignesh.palanisamy@solitontech.com>
2025-05-02 12:25:18 +0530

    feat: develop finance tracker functionalities for existing user

---
+++ Assignment-4/FinanceTracker/ExistingUser.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClosedXML.Excel;
+
+namespace FinanceTracker
+{
+    internal class ExistingUser
+    {
+        string filepath;
+        static XLWorkbook workbook;
+        static IXLWorksheet incomeSheet;
+        static IXLWorksheet expenseSheet;
+        public ExistingUser(string filePath)
+        {
+            filepath = filePath;
+            workbook = new XLWorkbook(filepath);
+            expenseSheet = workbook.Worksheet("Expense");
+            incomeSheet = workbook.Worksheet("Income");
+        }
+
+        public bool IsUser(string name)
+        {
+            bool incomeAvailable = incomeSheet.RowsUsed().Skip(1).Any(row => row.Cell(2).GetString().Equals(name, StringComparison.OrdinalIgnoreCase));
+            bool expenseAvailable = expenseSheet.RowsUsed().Skip(1).Any(row => row.Cell(2).GetString().Equals(name, StringComparison.OrdinalIgnoreCase));
+            if (!incomeAvailable && !expenseAvailable)
+            {
+                Console.WriteLine("User Not Found");
+                return false;
+            }
+            return true;
+
+        }
+
+
+        public void UserFunctions(string name, FinanceTransactions financer)
+        {
+            if (IsUser(name))
+            {
+
+                bool exit = false;
+                while (!exit)
+                {
+                    Console.WriteLine("1.Income Tracker\n2.Expense Tracker\n3.Finance Summarry\n4.Exit");
+
+                    int _choice = Validation.GetValidInteger("your choice");
+
+                    switch (_choice)
+                    {
+                        case 1:
+                            Income.IncomeFunctions(financer, name, filepath);
+                            break;
+                        case 2:
+                            Expense.ExpenseFunctions(financer, name, filepath);
+                            break;
+                        case 3:
+                            financer.FinanceSummary(name, filepath);
+                            break;
+                        case 4:
+                            Console.WriteLine("Exiting....");
+                            exit = true;
+                            break;
+                        default:
+                            Console.WriteLine("Please choose from given options");
+                            break;
+
+
+                    }
+                    if (_choice > 2)
+                    {
+                        Console.WriteLine("Press any key to continue.....");
+                        Console.ReadKey();
+                        Console.Clear();
+                    }
+                }
+
+            }
+
+
+        }
+    }
+}
--- Assignment-4/FinanceTracker/FinanceTransactions.cs
+++ Assignment-4/FinanceTracker/FinanceTransactions.cs
@@ -34,9 +34,165 @@ namespace FinanceTracker
 
             }
         }
-        public void EditTransaction(Transaction transaction, string filepath, string worksheetname)
+
+
+
+        public bool ViewTransaction(string name, string filepath, string worksheetname, int editIndex = -1)
         {
-            Console.WriteLine("Trying conflicts when changes made to parent branch");
+            int c = 0;
+            using (var workbook = new XLWorkbook(filepath))
+            {
+                var worksheet = workbook.Worksheet(worksheetname);
+                var rows = worksheet.RowsUsed().Skip(1).Where(r => r.Cell(2).GetString().Equals(name, StringComparison.OrdinalIgnoreCase));
+                if (!rows.Any())
+                { Console.WriteLine($"Sorry there are no {worksheetname} Transactions for {name} ."); return false; }
+                else
+                {
+                    Console.WriteLine($"{"ID",-10}{"Date",-30}{(worksheetname.Equals("Income") ? "income_source" : "expense_category"),5}{"Amount",15}");
+                    foreach (var row in rows)
+                    {
+                        c++;
+                        string date = row.Cell(1).GetDateTime().ToString();
+                        string sourceORcategory = row.Cell(3).GetString();
+                        double amount = row.Cell(4).GetDouble();
+                        if (c == editIndex)
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkYellow;
+                            Console.WriteLine($"{c,-10}{date,-30}{sourceORcategory,10}{amount,20}  (edited)");
+                        }
+                        else
+                        {
+
+                            Console.ForegroundColor = worksheetname.Equals("Income") ? ConsoleColor.Green : ConsoleColor.Red;
+                            Console.WriteLine($"{c,-10}{date,-30}{sourceORcategory,10}{amount,20}");
+                        }
+
+                        Console.ResetColor();
+                    }
+                    return true;
+                }
+            }
+        }
+
+        public void EditTransaction(string name, string filepath, string worksheetname)
+        {
+            using (var workbook = new XLWorkbook(filepath))
+            {
+                if (ViewTransaction(name, filepath, worksheetname))
+                {
+
+                    int id = Validation.GetValidInteger("id of the transaction you wish to edit :");
+                    var worksheet = workbook.Worksheet(worksheetname);
+                    var rows = worksheet.RowsUsed().Skip(1).Where(r => r.Cell(2).GetString().Equals(name, StringComparison.OrdinalIgnoreCase));
+                    int count = 0;
+                    if (id > rows.Count() || id < 1)
+                    {
+                        Console.WriteLine("Sorry id not found....");
+                    }
+                    else
+                    {
+                        foreach (var row in rows)
+                        {
+                            count++;
+                            if (count == id)
+                            {
+
+                                row.Cell(3).Value = Validation.GetValidString($"{(worksheetname.Equals("Income") ? "revised income source" : "revised expense category")}");
+                                row.Cell(4).Value = Validation.GetValidAmount();
+                                break;
+                            }
+                        }
+                        var range = worksheet.Range(2, 1, worksheet.LastRowUsed().RowNumber(), worksheet.LastColumnUsed().ColumnNumber());
+                        range.Sort("A", XLSortOrder.Ascending);
+                        workbook.Save();
+                        ViewTransaction(name, filepath, worksheetname, id);
+
+                        Console.WriteLine("\nEdited Succesfully......!!!");
+                    }
+                }
+            }
+
+        }
+
+        public void DeleteTransaction(string name, string filepath, string worksheetname)
+        {
+            using (var workbook = new XLWorkbook(filepath))
+            {
+                if (ViewTransaction(name, filepath, worksheetname))
+                {
+                    int id = Validation.GetValidInteger("id of the transaction you wish to delete :");
+                    var worksheet = workbook.Worksheet(worksheetname);
+                    var rows = worksheet.RowsUsed().Skip(1).Where(r => r.Cell(2).GetString().Equals(name, StringComparison.OrdinalIgnoreCase));
+                    if (id > rows.Count() || id < 1)
+                    {
+                        Console.WriteLine("Sorry id not found....");
+                    }
+                    else
+                    {
+                        int count = 0;
+                        foreach (var row in rows)
+                        {
+                            count++;
+                            if (count == id)
+                            {
+                                Console.WriteLine($"{row.Cell(1).GetDateTime(),-30}{row.Cell(3).GetString(),10}{row.Cell(4).GetDouble(),20}\n");
+                                string ch = "a";
+                                while (!ch.Equals("y") && !ch.Equals("Y") && !ch.Equals("n") && !ch.Equals("N"))
+                                {
+                                    Console.WriteLine($"Do you wish to delete this {worksheetname} transaction?[y/n]");
+                                    ch = Validation.GetValidString("choice");
+                                }
+                                if (ch.Equals("y") || ch.Equals("Y"))
+                                {
+                                    row.Delete();
+                                    Console.WriteLine("Deletion Successful...");
+                                }
+                                else
+                                    Console.WriteLine("Cancelling Delete...");
+                                break;
+                            }
+                        }
+                        workbook.Save();
+                        ViewTransaction(name, filepath, worksheetname);
+                    }
+                }
+
+            }
+
+        }
+
+        public void FinanceSummary(string name, string filepath)
+        {
+            var FinanceList = new List<(DateTime date, string type, string source, double amount)>();
+            double netBalance = 0;
+            using (var workbook = new XLWorkbook(filepath))
+            {
+                var incomeworksheet = workbook.Worksheet("Income");
+                var incomerows = incomeworksheet.RowsUsed().Skip(1).Where(r => r.Cell(2).GetString().Equals(name, StringComparison.OrdinalIgnoreCase));
+                var expenseworksheet = workbook.Worksheet("Expense");
+                var expenserows = expenseworksheet.RowsUsed().Skip(1).Where(r => r.Cell(2).GetString().Equals(name, StringComparison.OrdinalIgnoreCase));
+                foreach (var row in incomerows)
+                {
+                    FinanceList.Add(new(row.Cell(1).GetDateTime(), "Income", row.Cell(3).GetString(), row.Cell(4).GetDouble()));
+                    netBalance += row.Cell(4).GetDouble();
+                }
+                foreach (var row in expenserows)
+                {
+                    FinanceList.Add(new(row.Cell(1).GetDateTime(), "Expense", row.Cell(3).GetString(), row.Cell(4).GetDouble()));
+                    netBalance -= row.Cell(4).GetDouble();
+                }
+                var sortedTransactions = FinanceList.OrderBy(t => t.date);
+                Console.WriteLine($"{"Date",-30}{"Type",-10}{"Category",10}{"Amount",25}");
+                foreach (var t in sortedTransactions)
+                {
+                    Console.ForegroundColor = t.type == "Income" ? ConsoleColor.Green : ConsoleColor.Red;
+                    Console.WriteLine($"{t.date,-30}{t.type,-10}{t.source,10}{t.amount,25}");
+                }
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"\nNET BALANCE  :  {netBalance}");
+                Console.ResetColor();
+            }
+
         }
     }
 }
