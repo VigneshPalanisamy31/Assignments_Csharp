f650e12054cafbbcec2554474b58c0a621c70ec4
VigneshPalanisamy31 <vignesh.palanisamy@solitontech.com>
2025-05-19 09:44:32 +0530

    feat: develop classes for Value and reference types, garbage collection and Idisposable demo respectively with required functionalities

---
+++ Assignment-11/GarbageCollection/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace GarbageCollection
+{
+    class LargeObject
+    {
+        int []array=new int[10];
+        String []stringarray=new String[10];
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting memory usage profiling...");
+
+            PrintMemory("\nBefore object creation");
+
+            CreateObjects();
+
+            PrintMemory("\nAfter object creation");
+
+            Console.WriteLine("\nTriggering GC.Collect...");
+            GC.Collect();
+
+            PrintMemory("\nAfter GC.Collect");
+
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey();
+        }
+
+        static void CreateObjects()
+        {
+            const int objectCount = 10000;
+            for (int i = 0; i < objectCount; i++)
+            {
+                LargeObject obj = new LargeObject();
+            }
+        }
+
+        static void PrintMemory(string label)
+        {
+            long totalMemory = GC.GetTotalMemory(false);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"{label}: {totalMemory/(1024*1024)}MB");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Assignment-11/ValueAndReferenceTypes/ReferenceType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueAndReferenceTypes
+{
+    internal class ReferenceType
+    {
+        public string referenceString;
+        public ReferenceType(string name)
+        {
+            this.referenceString = name;
+        }
+    }
+}
