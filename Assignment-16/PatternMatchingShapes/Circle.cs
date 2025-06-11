afcb0fdfa603d585f93b3bd6895b658a8a86ae6e
Vignesh Palanisamy <vignesh.palanisamy@solitontech.local>
2025-06-11 12:18:37 +0530

    feat: develop functionalities to work with shapes and records

---
+++ Assignment-16/PatternMatchingShapes/Circle.cs
@@ -0,0 +1,13 @@
+﻿namespace PatternMatchingShapes
+{
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+        public Circle(double radius, string colour)
+        {
+            Radius = radius;
+            Colour = colour;
+        }
+        public override double CalculateArea() => Math.PI * Radius * Radius;
+    }
+}
---
+++ Assignment-16/PatternMatchingShapes/Rectangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternMatchingShapes
+{
+    public class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height, string colour)
+        {
+            Width = width;
+            Height = height;
+            Colour = colour;
+        }
+
+        public override double CalculateArea() => Width * Height;
+    }
+}
