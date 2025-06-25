c8a446bcf06d91b98508b74ba18b33ef4a6657d8
Vignesh Palanisamy <vignesh.palanisamy@solitontech.local>
2025-06-25 14:30:44 +0530

    feat: implementation of tasks in assignment_11

---
+++ Assignment-11/IDisposableDemo/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace IDisposableDemo
+{
+    public class Program
+    {
+        /// <summary>
+        /// Function to display countdown.
+        /// </summary>
+        /// <param name="displayMessage">Message to display to the console</param>
+        public static void CountDown(string displayMessage)
+        {
+            Console.Write($"\nCount down to complete {displayMessage} : ");
+            for (int time = 3; time > 0; time--)
+            {
+                Thread.Sleep(1000);
+                Console.Write($"{time} ");
+            }
+        }
+        /// <summary>
+        /// Function to display the completion of the file operation.
+        /// </summary>
+        /// <param name="fileOperation">File operation (Reading/Writing)</param>
+        public static void DisplaySuccessMessage(string fileOperation)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"\nFile {fileOperation} completed successfully!!");
+            Console.ResetColor();
+        }
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("-------- Handling File Operations --------");
+                Console.ResetColor();
+                string filename = "file.txt";
+                string text = "File Writing Success!!";
+                Console.WriteLine("\nWriting file...");
+                CountDown("Writing ");
+                using (FileWriter fileWriter = new FileWriter(filename))
+                {
+                    fileWriter.Write(text);
+                }
+                DisplaySuccessMessage("Writing");
+                Console.WriteLine("\nReading from a file...");
+                CountDown("Reading");
+                // Display message for the contents read from file
+                Console.WriteLine("\nFile Contents: ");
+                Console.WriteLine("\n-------- Contents from the file --------\n");
+                StreamReader streamReader = new StreamReader(filename);
+                Console.WriteLine(streamReader.ReadToEnd());
+                DisplaySuccessMessage("Reading");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception:{ex.Message}");
+            }
+        }
+    }
+}
---
+++ Assignment-11/ValueAndReferenceTypes/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace ValueAndReferenceTypes
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Function to modify the valuetype and referencetype variables.
+        /// </summary>
+        /// <param name="valueType">A value type parameter(int)</param>
+        /// <param name="referenceType">A reference type </param>
+        public static void Modify(int valueType,ReferenceType referenceType)
+        {
+            valueType = 200;
+            referenceType.ReferenceString = "Double Century";
+        }
+
+        /// <summary>
+        /// Function to create an array of integers.
+        /// </summary>
+        /// <param name="size">Size of an array</param>
+        public static void CreateArray(int size)
+        {
+            int []arrray=new int[size];
+            for (int i = 0;i < size;i++)
+                arrray[i]=i;
+        }
+        /// <summary>
+        /// Function to perform calculations with local value types.
+        /// </summary>
+        public static void PerformCalculations()
+        {
+            int num1 = 11, num2 = 22, num3 = 33, num4 = 44, num5 = 555, num6 = 66, num7 = 77, num8 = 88, num9 = 99, num10=111;
+            int product = num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num9 * num10;
+        }
+        public static void Main(string[] args)
+        {
+            try
+            {
+                int valueType = 100;
+                ReferenceType referenceType = new ReferenceType("Century");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"Values of value type and reference type before method call :\n");
+                Console.ResetColor();
+                Console.WriteLine($"ValueType (int) : {valueType}\n" + $"\nReferenceType (class) : {referenceType.ReferenceString}");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nCalling method to modify both....\n");
+                Console.ResetColor();
+
+                Modify(valueType, referenceType);
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"Values of value type and reference type before after call :\n");
+                Console.ResetColor();
+                Console.WriteLine($"ValueType (int) : {valueType}\n" + $"\nReferenceType (class) : {referenceType.ReferenceString}");
+                CreateArray(1000000);
+                PerformCalculations();
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+        }
+    }
+}
