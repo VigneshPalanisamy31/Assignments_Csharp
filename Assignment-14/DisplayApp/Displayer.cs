04610ad925e89bdf51a24931e3d9c41915c4d1a8
Vignesh Palanisamy <vignesh.palanisamy@solitontech.local>
2025-06-25 16:09:55 +0530

    feat: implement functions required for assignment_14

---
+++ Assignment-14/DisplayApp/Displayer.cs
@@ -0,0 +1,63 @@
+﻿using UtilityApp;
+using ProjectE;
+namespace DisplayApp
+{
+    public class Displayer
+    {
+        public IMathHelper mathUtilHelper;
+        public Displayer(IMathHelper mathHelper)
+        {
+            mathUtilHelper = mathHelper;
+            ConsoleDisplay();
+        }
+        /// <summary>
+        /// Displays menu to the user
+        /// </summary>
+        public void ConsoleDisplay()
+        {
+            bool isExit = false;
+            float firstNumber;
+            float secondNumber;
+            while (!isExit)
+            { 
+                Console.WriteLine("========Basic ArithMetics========");
+                Console.WriteLine("\n\n1.Add\n2.Subtract\n3.Multiply\n4.Divide\n5.Exit\n");
+                Console.WriteLine("Enter your choice:");
+                int choice = Helper.GetValidChoice(Console.ReadLine());
+                if (choice > 0 && choice < 5)
+                {
+                    Console.WriteLine("\n\nEnter Number 1: ");
+                    firstNumber = Helper.GetValidNumber(Console.ReadLine());
+                    Console.WriteLine("\nEnter Number 2: ");
+                    secondNumber = Helper.GetValidNumber(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine($"The sum of {firstNumber} and {secondNumber} is {mathUtilHelper.CalculateSum(firstNumber, secondNumber)}");
+                            break;
+
+                        case 2:
+                            Console.WriteLine($"The difference between {firstNumber} and {secondNumber} is {mathUtilHelper.CalculateDifference(firstNumber, secondNumber)}");
+                            break;
+
+                        case 3:
+                            Console.WriteLine($"The product of {firstNumber} and {secondNumber} is {mathUtilHelper.CalculateProduct(firstNumber, secondNumber)}");
+                            break;
+
+                        case 4:
+                            if (mathUtilHelper.CalculateQuotient(firstNumber, secondNumber) != 0)
+                                Console.WriteLine($"The quotient of {firstNumber} and {secondNumber} is {mathUtilHelper.CalculateQuotient(firstNumber, secondNumber)}");
+                            break;
+                    }
+                }
+                else if(choice==5)
+                   isExit = true;
+                else
+                   Console.WriteLine("Please enter a valid choice\n");
+                Console.WriteLine("\nPress any key to continue...");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
---
+++ Assignment-14/MathApp/MathUtils.cs
@@ -0,0 +1,60 @@
+﻿using ProjectE;
+using DisplayApp;
+namespace MathApp
+{
+    public class MathUtils:IMathHelper
+    {
+        public MathUtils()
+        {
+            Displayer displayer = new Displayer(this);
+        }
+        /// <summary>
+        /// Function to calculate sum of two numbers
+        /// </summary>
+        /// <param name="firstNumber">First number</param>
+        /// <param name="secondNumber">Second number</param>
+        /// <returns>Sum of given two numbers</returns>
+        public float CalculateSum(float firstNumber, float secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+        /// <summary>
+        /// Function to calculate difference of two numbers
+        /// </summary>
+        /// <param name="firstNumber">First number</param>
+        /// <param name="secondNumber">Second number</param>
+        /// <returns>Difference of given two numbers</returns>
+        public float CalculateDifference(float firstNumber, float secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+        /// <summary>
+        /// Function to calculate product of two numbers
+        /// </summary>
+        /// <param name="firstNumber">First number</param>
+        /// <param name="secondNumber">Second number</param>
+        /// <returns>product of given two numbers</returns>
+        public float CalculateProduct(float firstNumber, float secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+        /// <summary>
+        /// Function to divide given two numbers
+        /// </summary>
+        /// <param name="firstNumber">First number</param>
+        /// <param name="secondNumber">Second number</param>
+        /// <returns> Quotient after division</returns>
+        public float CalculateQuotient(float firstNumber, float secondNumber)
+        {
+            try
+            {
+                return firstNumber / secondNumber;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Execution interrupted due to an attempt to divide by 0 +\n{e.Message}");
+                return 0;
+            }
+        }
+    }
+}
